f3b46bf92f7ae3d43d37f023c50de1af9d7f2c1f
Setsoto Boleme <setsotoboleme@gmail.com>
2022-02-08 06:57:44 +0200

    Added output

--- js/basica.js
+++ js/basica.js
@@ -1,5 +1,65 @@
+let operation = ""
+let operand
 $(document).ready(function(){
     var start_width = $(".number").width()
     $(".number").css("height", start_width+"px")
     //alert(start_width)
+
+    $(".number").on("click", function(){
+        var vali = $(this).attr("value")
+        if ($(this).hasClass("operator")){
+            appendOperator(vali)
+        }
+        else
+            appendDisplay(vali)
+    })
+
+    $(".number.operator").on("click", function(){
+        
+    })
 })
+
+function appendDisplay(intt){
+    var content = $(".computing-display").html()
+    if (content=="0"){
+        $(".computing-display").html(intt)  
+    }
+    else{
+        $(".computing-display").html(content+intt)
+    }
+    
+    $("#operand").val($(".computing-display").html())
+    operand = $("#operand").val()
+    var result
+
+    if (operand!=="" && operation!==""){ // operate
+        if (operation==="mul")
+            result = +operand*+content
+
+        alert(operand)
+    }
+}
+
+function appendOperator(operator){
+    var content = $(".computing-display").html()
+    
+    if (content=="0"){
+
+    }else{
+        
+        var sign
+        if (operator==="mul"){
+            sign = "x"
+            operation = operator
+            operand = $("#operand").val()
+            $("#operand").val("")
+        }
+        alert(operand)
+            
+        $(".computing-display").html(content+sign)
+    }
+
+    
+}
+
+
