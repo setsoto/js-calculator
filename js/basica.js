2aca31e998e485a635f6a9e0d0d0c8f8ec5b8fc4
Setsoto Boleme <setsotoboleme@gmail.com>
2022-03-16 09:33:49 +0200

    Added basic operations

--- js/basica.js
+++ js/basica.js
@@ -1,6 +1,7 @@
 let operation = ""
 let operand
 $(document).ready(function(){
+    $('#operations-form')[0].reset()
     var start_width = $(".number").width()
     $(".number").css("height", start_width+"px")
     //alert(start_width)
@@ -25,19 +26,23 @@ function appendDisplay(intt){
         $(".computing-display").html(intt)  
     }
     else{
-        $(".computing-display").html(content+intt)
+        if ($("#state").val()==="evaluated")
+            $(".computing-display").html(intt)
+        else
+            $(".computing-display").html(content+intt)
+    }
+
+
+    if ($("#operand").val()!=="" && $("#operator").val()!==""){
+        $("#operand-2").val(intt)
+    }
+    else{
+        $("#operand").val($(".computing-display").html())
+        operand = $("#operand").val()
     }
     
-    $("#operand").val($(".computing-display").html())
-    operand = $("#operand").val()
-    var result
-
-    if (operand!=="" && operation!==""){ // operate
-        if (operation==="mul")
-            result = +operand*+content
-
-        alert(operand)
-    }
+    $("#state").val("")
+   
 }
 
 function appendOperator(operator){
@@ -48,17 +53,64 @@ function appendOperator(operator){
     }else{
         
         var sign
-        if (operator==="mul"){
-            sign = "x"
-            operation = operator
-            operand = $("#operand").val()
-            $("#operand").val("")
+        
+        switch (operator){
+            case 'mul':
+                sign = "x"
+                break
+            case 'min':
+                sign = "-"
+                break
+            case 'plus':
+                sign = "+"
+                break
+            case 'div':
+                sign = "&#247;"
+                break
         }
-        alert(operand)
-            
-        $(".computing-display").html(content+sign)
-    }
+        
+        if (operator==="eval"){
+            var operatr = $("#operator").val()
+            var op1 = $("#operand").val()
+            var op2 = $("#operand-2").val()
 
+            var result
+
+            switch ($("#operator").val()){
+                case 'mul':
+                    result = +op1*+op2
+                    break
+                case 'min':
+                    result = +op1-+op2
+                    break
+                case 'plus':
+                    result = +op1+(+op2)
+                    break
+                case 'div':
+                    result = +op1/(+op2)
+                    break
+            }
+            $(".computing-display").html(result)
+            // make result op1
+            $("#operand").val(result)
+            // clean out other fields
+            $("#operand-2").val("")
+            $("#operator").val("")
+
+            $("#state").val("evaluated")
+
+        }else{
+            $("#operator").val(operator)
+            $(".computing-display").html(content+sign)
+            $("#state").val("")
+        }
+
+        if (operator==="clear"){
+            $(".computing-display").html("0")
+            $('#operations-form')[0].reset()
+        }
+            
+    }
     
 }
 
